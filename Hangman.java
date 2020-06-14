f2d48dfb7418cddad9924157912a8a00a282c2aa
Nhien (Ricky) Lam <62775270+NhienLam@users.noreply.github.com>
2020-06-14 13:01:48 -0700

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -1,9 +1,6 @@
 package hangmanGame;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
